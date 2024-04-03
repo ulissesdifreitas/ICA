2bddf25d4b63ccf7fef173aee808937317b11e1f
Ulisses <ulisses1991@gmail.com>
2024-04-02 21:43:01 -0300

    add comments

--- cesta_basica.py
+++ cesta_basica.py
@@ -1,50 +1,51 @@
 
-from geneticEDIT import *
+from geneticEDIT import *   #importa as funções executadas no algoritmo genético.
 
-A = 'arroz'
-B = 'feijão'
-C = 'café'
-D = 'leite'
-E = 'macarrão'
-F = 'óleo'
-G = 'açúcar'
-H = 'farinha'
-I = 'biscoito'
-J = 'chocolate'
+A = '1.arroz'
+B = '2.feijão'
+C = '3.café'
+D = '4.leite'
+E = '5.macarrão'          # descrimina cada item possível da cesta básica .
+F = '6.óleo'
+G = '7.açúcar'
+H = '8.farinha'
+I = '9.biscoito'
+J = '10.chocolate'
 
-#EXIBINDO LISTA COMPLETA
+#LISTA COMPLETA
 listaItens = [A, B, C, D, E, F, G, H, I, J]
-listaValores = [[25], [25], [15], [15], [10], [10], [5], [5], [25], [25]]
+listaValores = [[25], [25], [15], [15], [10], [10], [5], [5], [25], [50]]       # define cada valor de cada item.
 listaCompleta = []
 
-for i in range(10):
-    listagem=(listaItens[i],listaValores[i])
-    listaCompleta.append(listagem)
-print(f'\nLista completa:\n\n{listaCompleta}\n')
 
 
-valor_maximo = 150
-n_de_cromossomos = 100
-geracoes = 50
-tamanho_da_lista = len(listaValores)
+valor_maximo = 150                      # valor máximo da cesta básica.
+n_de_cromossomos = 100                  # número de cromossomos/ indíviduos por geração.
+geracoes = 50                           # número de gerações.
+tamanho_da_lista = len(listaValores)    # tamanho da lista item/valor.
 
 
 #POPULAÇÃO
-populacao = population(n_de_cromossomos, tamanho_da_lista)
-historico_de_fitness = [media_fitness(populacao, valor_maximo, listaValores)]
+populacao = population(n_de_cromossomos, tamanho_da_lista)  #gera população aleatória.
+historico_de_fitness = [media_fitness(populacao, valor_maximo, listaValores)] #atribuição de valor e calculo a média de pontuação da geração, e armazena no histórico.
 for i in range(geracoes):
-    populacao = evolve(populacao, valor_maximo, listaValores, n_de_cromossomos)
-    historico_de_fitness.append(media_fitness(populacao, valor_maximo, listaValores))
+    populacao = evolucao(populacao, valor_maximo, listaValores, n_de_cromossomos) # capta os indivíduos e providencia a nova geração.
+    historico_de_fitness.append(media_fitness(populacao, valor_maximo, listaValores)) # atribui valor para a nova geração e armazena no histórico, até finalizar.
 
 
 
 for indice,dados in enumerate(historico_de_fitness):
-   print ("Geracao: ", indice," | Media de valor na mochila: R$", dados)
+   print ("Geracao: ", indice," | Media de valor da cesta básica: R$", dados)  # exibe as gerações 
 
 print("\nValor máximo:  R$",valor_maximo,"\n\nItens disponíveis:")
-for indice,i, in enumerate(listaValores) : 
+for indice,i, in enumerate(listaValores) :                                  # exibe valor máximo, itens disponíveis e valores 
         print("Item ",indice+1,":| R$",i)
+
+for i in range(10):
+    listagem=(listaItens[i],listaValores[i])
+    listaCompleta.append(listagem)
+print(f'\nLista completa:\n\n{listaCompleta}\n')        # exibe a listagem dos itens possíveis para a cesta básica, associando cada item ao seu valor.
     
-print("\nExemplos de boas solucoes: ")
+print("\nExemplos de boas solucoes: ")                  # exibe as cinco melhores soluções
 for i in range(5):
     print(populacao[i])
